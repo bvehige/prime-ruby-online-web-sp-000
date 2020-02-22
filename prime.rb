bd10bee7b8b8fc6920e87c3b0d1cc518ae5206ef
Brian Vehige <brianvehige@gmail.com>
2020-02-22 09:11:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(num)
+  if num <= 1 
+    return false 
+  elsif num <= 3 
+    return true 
+  else   
 (2...(num-1)).each do |n|
-  return false if num % n == 0 || if num <= 1
+  return false if num % n == 0 
 end
-  true
-
+end
 end
